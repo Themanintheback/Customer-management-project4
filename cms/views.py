1e8d0f934e8051040068aad81c55e82d0817bc99
Themanintheback <jonathan.john2501@hotmail.co.uk>
2022-10-01 13:53:19 +0000

    redered templates

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'cms/dashboard.html')
+
+
+def products(request):
+    return render(request, 'cms/products.html')
+
+
+def customer(request):
+    return render(request, 'cms/customer.html')
