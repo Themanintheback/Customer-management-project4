5aad3c69ad148fba8b325a9049869e65c44b16b5
Themanintheback <jonathan.john2501@hotmail.co.uk>
2022-10-01 10:07:00 +0000

    fix deployment error

--- accounts/settings.py
+++ accounts/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['accounts.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['customer-managment-system.herokuapp.com', 'localhost']
 
 
 # Application definition
